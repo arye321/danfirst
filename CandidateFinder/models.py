753fcef73688997727662086d1d63f9ee1bab76e
Aryep <lazylion2@gmail.com>
2021-12-13 16:27:12 +0200

    fixed bug with skill

--- CandidateFinder/models.py
+++ CandidateFinder/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Skill(models.Model):
     skill = models.CharField(max_length=40)
     def __str__(self):
-        return self.skillask
+        return self.skill
 class Candidate(models.Model):
     
     name = models.CharField(max_length=60)
